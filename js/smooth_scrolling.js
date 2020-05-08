a43fe69ffedec0fb59ebe6f305b6cbb662658812
kyonshou <bayugazetto@yahoo.co.id>
2020-05-08 13:18:01 +0700

    add page
    nice

---
+++ js/smooth_scrolling.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+/*$(document).on('click', 'a[href^="#"]', function(){
+	"use strict";
+	event.preventDefault();
+	
+	$("html, body").animate({
+		scrollTop: $($.attr(this, "href")).offset().top}, 1000);
+});*/
+
+$(document).ready(function(){
+	"use strict";
+	
+// Add smooth scrolling to all links
+$("a.sCroll").on('click', function(event) {
+
+	// Make sure this.hash has a value before overriding default behavior
+	if (this.hash !== "") {
+	// Prevent default anchor click behavior
+	event.preventDefault();
+
+	// Store hash
+	var hash = this.hash;
+
+	// Using jQuery's animate() method to add smooth page scroll
+	// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top
+	}, 1000, function(){
+
+		// Add hash (#) to URL when done scrolling (default click behavior)
+		window.location.hash = hash;
+	});
+	} // End if
+	});
+});
